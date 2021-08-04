17c64efeaa786e4cd329ee12ed1a426e4934534a
Benoit Serratrice <bserratrice@vmware.com>
2021-08-04 14:55:40 +0800

    update alarm to disable

--- labstartup/LabStartup-Step2.ps1
+++ labstartup/LabStartup-Step2.ps1
@@ -3,7 +3,7 @@
 
 # Fix DRS issues
 Write-Output "Check DRS configuration issues"
-Get-VMHost | Where-Object {$_.ExtensionData.ConfigIssue.FullFormattedMessage -like "Unable to apply DRS*"} | ForEach-Object {
+Get-VMHost | Where-Object { $_.ExtensionData.ConfigIssue.FullFormattedMessage -like "Unable to apply DRS*" } | ForEach-Object {
     Write-Output "DRS configuration issue for host $($_.Name), restarting hostd service."
     Invoke-Plink -remoteHost $_.Name -login root -passwd VMware1! -command "/etc/init.d/hostd restart"
     Do {
@@ -17,26 +17,20 @@ Get-Datastore esx-02-wcp-supervisor-vm | Get-AlarmDefinition -Name "Datastore us
 Get-Datastore esx-03-wcp-supervisor-vm | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
 Get-Datastore esx-04-wcp-supervisor-vm | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
 
-Write-Output "Disable Host memory usage alarm"
-Get-VMHost esx-01.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-02.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-03.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-04.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
-
 Write-Output "Disable vSAN Alarm"
 Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN Health Alarm 'Disks usage on storage controller'" | Set-AlarmDefinition -enabled:$false | Out-Null
 Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN Health Alarm 'vSAN max component size'" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN health alarm 'vSAN disk balance'" | Set-AlarmDefinition -enabled:$false | Out-Null
 Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN Support Insight" | Set-AlarmDefinition -enabled:$false | Out-Null
 Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN hardware compatibility issues" | Set-AlarmDefinition -enabled:$false | Out-Null
 Get-Cluster "VMware-Cloud" | Get-AlarmDefinition -Name "vSAN health alarm 'Performance service status'" | Set-AlarmDefinition -enabled:$false | Out-Null
 
-Write-Output "Disable Network Alarms"
-Get-VMHost esx-01.corp.local | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-02.corp.local | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-03.corp.local | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-04.corp.local | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-
-Get-VMHost esx-01.corp.local | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-02.corp.local | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-03.corp.local | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$false | Out-Null
-Get-VMHost esx-04.corp.local | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$false | Out-Null
+Write-Output "Disable Host memory usage alarm and reset Network Alarms"
+Get-VMHost | ForEach-Object {
+    $_ | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
+    $_ | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$false | Out-Null
+    $_ | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$false | Out-Null
+    Start-Sleep -Seconds 1
+    $_ | Get-AlarmDefinition -Name "Network connectivity lost" | Set-AlarmDefinition -enabled:$true | Out-Null
+    $_ | Get-AlarmDefinition -Name "Network uplink redundancy lost" | Set-AlarmDefinition -enabled:$true | Out-Null
+}
