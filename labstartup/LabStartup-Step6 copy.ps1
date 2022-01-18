ba7e89d94ca7d413a89ecb34a6ac7f7809b19bab
Benoit Serratrice <bserratrice@vmware.com>
2022-01-18 16:37:23 +0800

    fix file duplicate

--- labstartup/LabStartup-Step6 copy.ps1
+++
@@ -1,4 +0,0 @@
-# Wait for VCD
-Write-VpodProgress "Check VCD" 'GOOD-8'
-$vcdCheckPath = Join-Path $PSScriptRoot "vcd-restart-vcenter-connection.py"
-python $vcdCheckPath
--- labstartup/LabStartup-Step6.ps1
+++ labstartup/LabStartup-Step6.ps1
@@ -1,4 +1,4 @@
 # Wait for VCD
-Write-VpodProgress "Check VCD"
+Write-VpodProgress "Check VCD" 'GOOD-8'
 $vcdCheckPath = Join-Path $PSScriptRoot "vcd-restart-vcenter-connection.py"
 python $vcdCheckPath
